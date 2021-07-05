74b158891cf21a16287ac63b3de4428f4a323a63
Joshua Rully <joshrully@gmail.com>
2021-07-05 13:17:17 -0400

    add api requests and spinner

--- src/context.jsx
+++ src/context.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: 'name' } },
-      { track: { track_name: 'test' } }
-    ],
+    track_list: [],
     heading: 'Top 10 Tracks'
+  };
+
+  componentDidMount() {
+    axios.get(`https://cors-access-allow.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&canada=it&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}
+    `)
+      .then(res => {
+        // console.log(res.data)
+        this.setState({track_list: res.data.message.body.track_list})
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
