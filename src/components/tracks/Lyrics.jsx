0a3ab9525204ab86e2221212d108d36a2299314c
Joshua Rully <joshrully@gmail.com>
2021-07-05 16:32:41 -0400

    fix undefined genre bug

--- src/components/tracks/Lyrics.jsx
+++ src/components/tracks/Lyrics.jsx
@@ -51,7 +51,11 @@ class Lyrics extends Component {
               <strong>Album ID</strong>: {track.album_id}
             </li>
             <li className="list-group-item">
-              <strong>Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+              <strong>Genre</strong>: {
+              track.primary_genres.music_genre_list[0].music_genre.music_genre_name ?
+              track.primary_genres.music_genre_list[0].music_genre.music_genre_name :
+              'No Genre'
+              }
             </li>
             <li className="list-group-item">
               <strong>Explicit</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
