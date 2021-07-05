78143b91be788ff181f6b325513b5e87ad70b457
Joshua Rully <joshrully@gmail.com>
2021-07-05 12:51:13 -0400

    add context to Tracks - mock data

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 
+import { Provider } from './context';
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar/>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <Provider>
+      <Router>
+        <>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </Provider>
   );
 }
 
